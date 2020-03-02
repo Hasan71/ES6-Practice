d6ed1c319c4307f6979eed83a980d0acfbec2c96
Hasan71 <mahmud.cse7@gmail.com>
2020-03-03 00:29:22 +0600

    adding letconst and default

---
+++ default.js
@@ -0,0 +1,15 @@
+function add (num1, num2=20){
+    
+    // if(num2==undefined){
+    //     num2 = 0;
+    // }
+
+    //num2 = num2 || 20;
+
+    return num1 + num2;
+
+}
+
+const sum = add(12,10);
+
+console.log(sum);
---
+++ letconst.js
@@ -0,0 +1,22 @@
+const hubby = "Ilias Kanchon";
+
+//hubby = "rubel"
+
+console.log(hubby);
+
+const numbers = [12, 34, 56, 78];
+
+numbers[3] = 100;
+
+console.log(numbers);
+
+//numbers = ["he", "she", "me"];
+
+console.log(numbers);
+
+let sum = 0;
+for(var i=0; i<10; i++){
+    sum = sum + i;
+}
+console.log(i);
+
