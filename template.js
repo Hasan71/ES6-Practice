bb5a76b35e821d39f99e87de630a545c3ff6aae8
Hasan71 <mahmud.cse7@gmail.com>
2020-03-03 00:52:08 +0600

    adding template

---
+++ template.js
@@ -0,0 +1,26 @@
+const firstName = "Justin";
+
+const lastName = "Timberlake";
+
+//const fullName  = firstName + " " + lastName + " is a good boy";
+
+const fullName  =  `${firstName} ${lastName} is a good boy`;
+
+console.log(fullName);
+
+
+const multiLine = "i love you\n" + 
+"i need you\n" + 
+"i miss you\n";
+
+console.log(multiLine);
+
+//no need to give + singn and \n for new line
+const multiLine2 = `i love you /
+i miss you
+i need you`;
+
+console.log(multiLine2);
+
+
+
